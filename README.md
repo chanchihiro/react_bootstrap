744ce5a14eb4789690a5e0febcd6111b1e4348d6
chanchi- <chanchi0707@gmail.com>
2017-01-03 00:37:04 +0900

    ファーストこみっつ

---
+++ README.md
@@ -0,0 +1,5 @@
+# 概要
+Reactのbootstrapを使います
+
+#動作
+
---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
+import Todo from "./components/Todo"
+
+
+ReactDOM.render(
+	<Todo />,
+	document.getElementById("content")
+);
